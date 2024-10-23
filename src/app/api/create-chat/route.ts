0d7a4a5938e770e20d86157cc4e6ace675e13836
devansh-mankar <Devanshmankar09@gmail.com>
2024-10-23 18:29:43 +0530

    changes

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -5,7 +5,7 @@ import { chats } from "@/lib/DB/schema";
 import { getS3Url } from "@/lib/s3";
 import { auth } from "@clerk/nextjs/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   const { userId } = await auth();
   if (!userId) {
     return NextResponse.json({ error: "unauthorized" }, { status: 401 });
